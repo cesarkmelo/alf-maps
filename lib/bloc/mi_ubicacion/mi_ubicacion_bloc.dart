c497a5607c5cc9fe186c936d08233f29c4b3d47a
Cesar Camelo <cesar@alfamediaweb.com>
2021-11-03 16:53:57 -0600

    Bloc de mi ubicación y archivos adicionales

---
+++ lib/bloc/mi_ubicacion/mi_ubicacion_bloc.dart
@@ -0,0 +1,37 @@
+import 'dart:async';
+
+import 'package:bloc/bloc.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:meta/meta.dart';
+
+part 'mi_ubicacion_event.dart';
+part 'mi_ubicacion_state.dart';
+
+class MiUbicacionBloc extends Bloc<MiUbicacionEvent, MiUbicacionState> {
+  MiUbicacionBloc() : super(MiUbicacionState());
+
+  // Obtengo posicion del usuario
+  StreamSubscription<Position>? _positionSubscription;
+  void iniciarSeguimiento() {
+    //Geolocator.isLocationServiceEnabled();
+
+    _positionSubscription = Geolocator.getPositionStream(
+            desiredAccuracy: LocationAccuracy.high, distanceFilter: 10)
+        .listen((Position position) {
+      final nuevaUbicacion = LatLng(position.latitude, position.longitude);
+      add(OnUbicacionCambio(nuevaUbicacion));
+    });
+  }
+
+  void cancelarSeguimiento() {
+    _positionSubscription?.cancel();
+  }
+
+  @override
+  Stream<MiUbicacionState> mapEventToState(MiUbicacionEvent event) async* {
+    if (event is OnUbicacionCambio) {
+      yield state.copyWith(existeUbicacion: true, ubicacion: event.ubicacion);
+    }
+  }
+}
